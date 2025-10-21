1a02e8eea8a3cb77de943cfc89a0c8e4c64d2fbe
NeilJames01 <neiljames22092001@gmail.com>
2025-10-21 14:53:33 +0100

    Add files via upload

---
+++ Week1_lab.py
@@ -0,0 +1,17 @@
+print("Hello World")
+# Storing text
+student_name = "Neil"
+
+# Storing a whole number
+student_age = 24
+
+#Print the variables to the console
+print("Student Name:", student_name)
+print("Student Age", student_age)
+
+# A simple calculation
+num1 = 15
+num2 = 20
+total = num1 + num2
+
+print("The sum is:", total)
